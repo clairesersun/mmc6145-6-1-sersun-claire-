d2adbbc384febe907d180a9a101f19b1b1a1b1e0
Claire Sersun <claire.sersun@gmail.com>
2023-03-30 15:27:35 -0400

    update user errors

--- pages/api/book.js
+++ pages/api/book.js
@@ -6,26 +6,28 @@ import db from '../../db'
 export default withIronSessionApiRoute(
   async function handler(req, res) {
     const user = req.session.user
-    if (!user) {
-      res.status(401).json({error:error.message}).clear().end()
-    } else {
     switch(req.method) {
-
       // TODO: On a POST request, add a book using db.book.add with request body (must use JSON.parse)
       case 'POST' :
+        if (!user) {
+          res.status(401).end()
+        } else {
           const add = await JSON.parse(db.book.add(req.body.task))
-          return res.status(200).json(add)
+          return res.status(200).json(add)}
           // TODO: On a DELETE request, remove a book using db.book.remove with request body (must use JSON.parse)
       case 'DELETE' :
+        if (!user) {
+          res.status(401).end()
+        } else {
         const remove = await JSON.parse(db.book.remove(req.body.task))
-        return res.status(200).json(remove)
+        return res.status(200).json(remove)}
       // TODO: Respond with 404 for all other requests
       default:
         return res.status(404).end()
         // User info can be accessed with req.session
         // No user info on the session means the user is not logged in
       }
-      }
+      
       
     },
     sessionOptions
